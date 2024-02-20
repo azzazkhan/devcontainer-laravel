e968435171d6b808dcd1e3592b4debaa331d48fa
Azzaz Khan <25636920+azzazkhan@users.noreply.github.com>
2024-02-20 12:17:09 +0500

    Removed Windows Powershell installation script

--- install.ps1
+++
@@ -1,9 +0,0 @@
-if (-not (Test-Path -Path ".\.devcontainer" -PathType Container)) {
-    New-Item -ItemType Directory -Path ".\.devcontainer" | Out-Null
-}
-
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/azzazkhan/devcontainer-laravel/master/.devcontainer/devcontainer.json" -OutFile ".\.devcontainer\devcontainer.json"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/azzazkhan/devcontainer-laravel/master/docker-compose.yml" -OutFile "docker-compose.yml"
-Invoke-WebRequest -Uri "https://github.com/azzazkhan/devcontainer-laravel/raw/master/sail" -OutFile "sail"
-
-Write-Host "Setup complete!"
